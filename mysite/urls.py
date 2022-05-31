3d097469ead023ee7c8e71f4026455e09ad46ed6
rs7dr <98549784+rs7dr@users.noreply.github.com>
2022-05-30 23:22:06 -0700

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('Room Finder/', include('room_finder.urls')),
+    path('room/', include('room.urls')),
     path('admin/', admin.site.urls),
 ]
