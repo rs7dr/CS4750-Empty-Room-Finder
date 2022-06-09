c50135ed6fe6f65dc1cb31173510532e93a30fc1
Eric Wang <tw3ad@virginia.edu>
2022-06-09 14:26:51 -0400

    edits to settings.py and html templates and urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
-    #path('room/', include('room.urls')),
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('Student/', include('Student.urls')),
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
+#from .models import Post
 
 def index(request):
     return HttpResponse("Hello, world. You're at the Empty Room Finder Homepage.")
-  
+
+
+#return http response or return a template.
+
+
+
+
